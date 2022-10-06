4e3d190feeba08d3d1532e3277d601044e76ec65
jihun <jihunv8@gmail.com>
2022-10-06 09:14:07 +0900

    [config] webpack 설정 수정

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
   },
-
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
   plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
 };
